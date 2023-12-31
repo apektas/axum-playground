a0a70b17794c8115d1ebad5f8af4a151f7bc20be
Abdurrahman Pektas <apektas@yandex.com>
2023-12-31 11:43:14 +0300

    Client Error mapping done

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,10 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+use strum_macros::AsRefStr;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, AsRefStr)]
 pub enum Error {
     LoginFail,
 
@@ -20,6 +21,46 @@ pub enum Error {
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES" );
-        (StatusCode::INTERNAL_SERVER_ERROR, "Un handled client error").into_response()
+
+        // Create a placeholder Axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+
+        response.extensions_mut().insert(self);
+        response
+
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Error::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // Auth
+            Error::AuthFailNoAuthTokenCookie
+            | Error::AuthFailCtxNotInRequestExtension
+            | Error::AuthFailTokenWrongFormat => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+            //Model
+            Error::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            // can be useful to have
+            #[allow(unreachable_patterns)]
+            _ => (
+            	StatusCode::INTERNAL_SERVER_ERROR,
+            	ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use self::error::{Error, Result};
 
 use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse, Response};
-use axum::{middleware, Router};
+use axum::{Json, middleware, Router};
 use axum::http::StatusCode;
 use axum::routing::{get, get_service};
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::{CookieManager, CookieManagerLayer, Cookies};
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 use crate::model::ModelController;
 
 
@@ -101,6 +103,36 @@ async fn handler_hello_path_variable(Path(name): Path<String>) -> impl IntoRespo
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+
+    let uuid = Uuid::new_v4();
+
+    // Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error =  service_error.map(|se| se.client_status_and_error());
+
+    // if
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("->> client_error_body: {client_error_body}");
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    //
+
+    println!("->> server log line {:<12} Error: {service_error:?}", uuid);
+
+
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
